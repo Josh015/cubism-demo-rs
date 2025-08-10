fa7e9766c3896dab2f06940c96e44c4207b16c7c
josh015 <joshua.d.ols@gmail.com>
2025-08-10 11:38:07 -0400

    Fix for error log spam on AMD

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,10 @@ mod systems;
 
 use bevy::{
     prelude::*,
+    render::{
+        RenderPlugin,
+        settings::{Backends, RenderCreation, WgpuSettings},
+    },
     window::{PresentMode, WindowResolution},
 };
 use serialization::*;
@@ -14,15 +18,25 @@ fn main() {
     let config: Config = load_config_from_ron_file("assets/config/demo.ron");
 
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: config.title.clone(),
-                resolution: WindowResolution::new(config.width as f32, config.height as f32),
-                present_mode: PresentMode::AutoVsync,
+        .add_plugins(
+            DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: config.title.clone(),
+                    resolution: WindowResolution::new(config.width as f32, config.height as f32),
+                    present_mode: PresentMode::AutoVsync,
+                    ..default()
+                }),
+                ..default()
+            })
+            .set(RenderPlugin {
+                render_creation: RenderCreation::Automatic(WgpuSettings {
+                    backends: Some(Backends::VULKAN),
+                    ..default()
+                }),
                 ..default()
             }),
-            ..default()
-        }))
+        )
         .insert_resource(ClearColor(Color::srgb(0.35, 0.35, 0.35)))
         // .add_plugin(PrintDiagnosticsPlugin::default())
         // .add_plugin(FrameTimeDiagnosticsPlugin::default())
